72f4a7047a741ed3c13e10ba9db79e1b4c839a5e
Hosilbek <hlochinbekov@gmail.com>
2023-05-12 22:45:01 -0700

    3rd

--- hosilbek.py
+++ hosilbek.py
@@ -3,10 +3,13 @@ daraja = int(input("daraja"))
 
 natija = 1
 
-while daraja != 0:
-    natija = natija*son
-    daraja=daraja-1
+# while daraja != 0:
+#     natija = natija*son
+#     daraja=daraja-1
 
+# print("jaVOB = " + str(natija))
+
+for i in range(daraja):
+    natija = natija * son
+    daraja = daraja - 1
 print("jaVOB = " + str(natija))
-
-
